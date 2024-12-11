84b358434b38ea8634d696f0904462adda4c38f6
Claire Castanet <clairecastanet@MASTAFF-Claire-Castanet-Q14CH4R1PW.local>
2024-12-11 12:48:14 +0000

    code to go with testing video

--- src/test/java/com/makers/makersbnb_coach_support_code/MakersbnbCoachSupportCodeApplicationTests.java
+++ src/test/java/com/makers/makersbnb_coach_support_code/MakersbnbCoachSupportCodeApplicationTests.java
@@ -1,12 +1,34 @@
 package com.makers.makersbnb_coach_support_code;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Before;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(properties = "spring.flyway.clean-disabled=false")
 class MakersbnbCoachSupportCodeApplicationTests {
 
-	@Test
+	@Autowired
+	private Flyway flyway;
+
+	@BeforeEach
+    void setUpDatabase() {
+		flyway.clean();
+		flyway.migrate();
+	}
+
+	@AfterEach
+	void cleanUpDatabase() {
+		flyway.clean();
+	}
+
+    @Test
 	void contextLoads() {
 	}
 
